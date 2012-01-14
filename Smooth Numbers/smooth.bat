a2c0400bcc71f4c6998dc5e2ea3f61855136aaf4
Nomad <gd.nightmare@gmail.com>
2012-01-14 23:34:49 +0200

    Added old VS2008 project to mercurial. (now smooth.c contains the solution) Renamed the project from 'smooth numbers' to 'Smooth Numbers' before committing.
    Original timestamps:
    smooth numbers.sln:                                     13/ 4/2010  4:38:57pm
    smooth numbers.vcproj:                                  23/ 6/2010  4:13:51am
    smooth numbers.vcproj.Nomad-Desktop.Nomad.user:         29/ 6/2010  2:36:03am
    smooth.bat:                                             23/ 6/2010  4:05:22am
    smooth.c:                                               13/ 4/2010  5:32:09pm

---
+++ Smooth Numbers/smooth.bat
@@ -0,0 +1,21 @@
+smooth.exe 2 1 42
+smooth.exe 3 42 420
+smooth.exe 7 33 192
+
+smooth.exe 5 5 10
+smooth.exe 4 5 10
+smooth.exe 3 5 10
+smooth.exe 11 17 25
+
+smooth.exe 512 120 50000
+smooth.exe 640 32 750000
+smooth.exe 780 1024 10240
+smooth.exe 1024 1 1011111
+
+smooth.exe 1024 1 1000000
+smooth.exe 1024 1 10000000
+smooth.exe 1024 1 100000000
+smooth.exe 1024 1 1000000000
+smooth.exe 1024 1 10000000000
+pause
+exit
--- Smooth Numbers/smooth.c
+++ Smooth Numbers/smooth.c
@@ -1,24 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static unsigned int smooth(unsigned int b, unsigned long long i, unsigned long long j)
+#define length(A) (sizeof(A)/sizeof(A[0]))
+typedef unsigned long long int ull;
+typedef unsigned int ui;
+
+ui primes[] = {
+	2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,
+	73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,
+	151,157,163,167,173,179,181,191,193,197,199,211,223,227,
+	229,233,239,241,251,257,263,269,271,277,281,283,293,307,
+	311,313,317,331,337,347,349,353,359,367,373,379,383,389,
+	397,401,409,419,421,431,433,439,443,449,457,461,463,467,
+	479,487,491,499,503,509,521,523,541,547,557,563,569,571,
+	577,587,593,599,601,607,613,617,619,631,641,643,647,653,
+	659,661,673,677,683,691,701,709,719,727,733,739,743,751,
+	757,761,769,773,787,797,809,811,821,823,827,829,839,853,
+	857,859,863,877,881,883,887,907,911,919,929,937,941,947,
+	953,967,971,977,983,991,997,1009,1013,1019,1021
+}; // primes
+
+
+ull i,j;
+ui b;
+ui smooths = 0;
+ui *primes_end;	// points just after the greatest prime <= b
+
+
+
+static void count_smooths(ui *prime,ull accumulated_product)
 {
-  /*
-   * PLACE YOUR CODE HERE...
-   */
-}
+	if(prime == primes_end)
+	{
+		if(accumulated_product <= j && accumulated_product >= i)
+			smooths++;
+	}
+	else
+		while(accumulated_product <= j)
+		{
+			count_smooths(prime+1,accumulated_product);
+			accumulated_product *= *prime;
+		} // end while
+} // end count_smooths
+
+
+
 
 int main(int argc, char* argv[])
 {
-  unsigned int  b;
-  unsigned long long i, j;
+	// read input
+	if(argc != 4)
+	{
+		printf("Usage: smooth b i j\n");
+		return 1;
+	} // end if
+	sscanf(argv[1], "%u",  &b);
+	sscanf(argv[2], "%lu", &i);
+	sscanf(argv[3], "%lu", &j);
 
-  if (argc != 4) {
-    printf("Usage: smooth b i j\n"); return 1;
-  }
-  sscanf(argv[1], "%u",  &b);
-  sscanf(argv[2], "%lu", &i);
-  sscanf(argv[3], "%lu", &j);
-  printf("%d\n", smooth(b, i, j));
-  return 0;
+	// preparations
+	primes_end = primes;
+	while(*primes_end <= b && primes_end < primes+length(primes))
+		primes_end++;
+
+	// actual work	
+	count_smooths(primes,1);
+
+	printf("%d\n",smooths);
+	return 0;
 }
