9893f8aea6684c1e10a24edff8afe31081ca64be
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-29 02:23:50 +0300

    Added copyright notices and license.

--- Smooth Numbers/smooth.bat
+++ Smooth Numbers/smooth.bat
@@ -1,3 +1,20 @@
+::	Copyright (C) 2010, 2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+::
+::	This file is part of Smooth Numbers.
+::
+::	Smooth Numbers is free software: you can redistribute it and/or modify
+::	it under the terms of the GNU General Public License as published by
+::	the Free Software Foundation, either version 3 of the License, or
+::	(at your option) any later version.
+::
+::	Smooth Numbers is distributed in the hope that it will be useful,
+::	but WITHOUT ANY WARRANTY; without even the implied warranty of
+::	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+::	GNU General Public License for more details.
+::
+::	You should have received a copy of the GNU General Public License
+::	along with Smooth Numbers.  If not, see <http://www.gnu.org/licenses/>.
+
 smooth.exe 2 1 42
 smooth.exe 3 42 420
 smooth.exe 7 33 192
